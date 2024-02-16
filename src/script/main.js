d3199a15dcb7f02e4fab2293a77e2803475a0c8a
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2024-02-15 22:41:15 -0300

    downs

--- src/script/main.js
+++ src/script/main.js
@@ -2,21 +2,35 @@
 
 const btn = document.querySelector('.btn-mode');
 const header = document.querySelector('.header');
-const aboutTitle = document.querySelector('.about-title');
 const main = document.querySelector('.main');
+const aboutTitle = document.querySelector('.about-title');
+const titleAboutMe = document.querySelector('.title-about-me');
+const btnMode = document.querySelector('.btn-mode');
+const btnCv = document.querySelector('#btn-cv');
+const downTitleAboutMe = document.querySelector('.down-title');
+const subtitleAboutMe = document.querySelector('.subtitle-about-me');
+const titleProjects = document.querySelector('.title-projects');
+const downTitleProjects = document.querySelector('.down-title-projects');
+const subtitleProjects = document.querySelector('.subtitle-projects');
+const titleContato = document.querySelector('.title-contato');
+const downContato = document.querySelector('.down-contato');
+const subtitleContato = document.querySelector('.subtitle-contato');
 
 btn.onclick = function () {
     this.classList.toggle('active')
     header.classList.toggle('active')
-    aboutTitle.classList.toggle('active')
     main.classList.toggle('active')
+    aboutTitle.classList.toggle('light-mode')
+    titleAboutMe.classList.toggle('title-about-me-light')
+    btnMode.classList.toggle('btn-light-mode')
+    btnCv.classList.toggle('btn-cv-light')
+    downTitleAboutMe.classList.toggle('down-title-light')
+    downTitleProjects.classList.toggle('down-title-light')
+    titleProjects.classList.toggle('title-about-me-light')
+    subtitleProjects.classList.toggle('subtitle-light')
+    subtitleAboutMe.classList.toggle('subtitle-light')
+    titleContato.classList.toggle('title-about-me-light')
+    downContato.classList.toggle('down-title-light')
+    subtitleContato.classList.toggle('subtitle-light')
 }
 
-// Icon transparent ao scrollar a pagina 
-/*
-    window.addEventListener("scroll", function() {
-        const icon = document.querySelector('.icon');
-
-        icon.classList.toggle('transparent', window.scrollY > 0);
-    });
-*/
